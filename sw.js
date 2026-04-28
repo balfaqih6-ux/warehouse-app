544d01cc7e36ca0d47382970a3221255f21bd8a4
balfaqih6-ux <balfaqih6@gmail.com>
2026-04-28 12:56:22 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = 'warehouse-v1';
+const FILES = [
+  '/',
+  '/index.html',
+  '/receiving.html',
+  '/audit.html',
+  '/admin.html',
+  '/config.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
