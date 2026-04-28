18c5b2cd3c3fd9bbdab265bcacab9e54b099a1a2
balfaqih6-ux <balfaqih6@gmail.com>
2026-04-28 12:00:03 +0300

    Add Supabase configuration to config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+const SUPABASE_URL = 'https://frptrubddbmwrheaeznh.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZycHRydWJkZGJtd3JoZWFlem5oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczMzI4MDgsImV4cCI6MjA5MjkwODgwOH0.XLjTAc597uDmH0eo2lwVHE8nE4UYrNGHUiO_tsSiOxk';
+
+const MAKE_WEBHOOK_RECEIVING = '';
+const MAKE_WEBHOOK_AUDIT = '';
